7a4c2de7b1c69f467ab2d90166c6320ed4f9949c
thlee <thlee@t5online.com>
2021-05-24 12:37:47 +0900

    2-1. _app.js로 레이아웃 분리하기

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -5,18 +5,26 @@ import { Form, Input, Checkbox, Button } from "antd";
 import { useState } from "react";
 
 const Signup = () => {
-  const [id, setId] = useState("");
   const [nick, setNick] = useState("");
   const [password, setPassword] = useState("");
   const [passwordCheck, setPasswordCheck] = useState("");
   const [term, setTerm] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [termError, setTermError] = useState(false);
 
-  const onSubmit = (event) => {
-    event.preventDefault();
+  const onFinish = (values) => {
+    if (password !== passwordCheck) {
+      return setPasswordError(true);
+    }
+    if (!term) {
+      return setTermError(true);
+    }
+    console.log("Success:", values);
+    console.log(id, nick, password, passwordCheck, term);
   };
 
-  const onChangeId = (e) => {
-    setId(e.target.value);
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
   };
 
   const onChangeNick = (e) => {
@@ -28,19 +36,36 @@ const Signup = () => {
   };
 
   const onChangePasswordCheck = (e) => {
+    setPasswordError(e.target.value !== password);
     setPasswordCheck(e.target.value);
   };
 
   const onChangeTerm = (e) => {
-    setTerm(e.target.value);
+    setTermError(false);
+    setTerm(e.target.checked);
   };
+
+  const useInput = (initValue = null) => {
+    const [value, setter] = useState(initValue);
+    const handler = (e) => {
+      setter(e.target.value);
+    };
+    return [value, handler];
+  };
+
+  const [id, onChangeId] = useInput("");
+
   return (
     <>
       <Head>
         <title>NodeBird</title>
       </Head>
       <AppLayout>
-        <Form onSubmit={onSubmit} style={{ padding: 10 }}>
+        <Form
+          style={{ padding: 10 }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+        >
           <div>
             <label htmlFor="user-id">아이디</label>
             <br />
@@ -77,13 +102,19 @@ const Signup = () => {
               required
               onChange={onChangePasswordCheck}
             />
+            {passwordError && (
+              <div style={{ color: "red" }}>비밀번호가 일치하지 않습니다.</div>
+            )}
           </div>
           <div>
-            <Checkbox name="user-term" value={term} onChange={onChangeTerm}>
-              동의 체크박스
+            <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
+              약관 동의 체크박스
             </Checkbox>
+            {termError && (
+              <div style={{ color: "red" }}>약관에 동의셔야 합니다.</div>
+            )}
           </div>
-          <div>
+          <div style={{ marginTop: 10 }}>
             <Button type="primary" htmlType="submit">
               가입하기
             </Button>
