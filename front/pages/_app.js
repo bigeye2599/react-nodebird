448047816697c8ae379452c4d1812c0b34fe535a
bigeye2599 <bigeye2599@gmail.com>
2021-05-29 19:27:37 +0900

    4-2. 사가 미들웨어 리덕스에 연결하기

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import withRedux from "next-redux-wrapper";
+import rootReducer from "../reducers";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "../sagas";
+import { composeWithDevTools } from "redux-devtools-extension";
 import Head from "next/head";
 import PropTypes from "prop-types";
 import "antd/dist/antd.css";
 import AppLayout from "../components/AppLayout";
-import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from "../reducers";
 
 const NodeBird = ({ Component }) => {
   return (
@@ -27,8 +29,13 @@ NodeBird.propTypes = {
 };
 
 export default withRedux((initialState, options) => {
-  const middlewares = [];
-  const enhancer = composeWithDevTools(applyMiddleware(...middlewares));
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware];
+  const enhancer =
+    process.env.NODE_ENV === "production"
+      ? compose(applyMiddleware(...middlewares))
+      : composeWithDevTools(applyMiddleware(...middlewares));
   const store = createStore(rootReducer, initialState, enhancer);
+  sagaMiddleware.run(rootSaga);
   return store;
 })(NodeBird);
