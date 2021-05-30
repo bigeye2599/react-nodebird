58a738daba0c547ad0a51e97e163c4b1bbad1b9e
bigeye2599 <bigeye2599@gmail.com>
2021-05-30 19:00:51 +0900

    4-13. 게시글 작성 리덕스 사이클

--- front/reducers/post.js
+++ front/reducers/post.js
@@ -12,6 +12,15 @@ export const initialState = {
   imagePaths: [], // 미리보기 이미지 경로
   addPostErrorReason: "", // 포스트 업로드 실패 사유
   isAddingPost: false, // 포스트 업로드 중
+  postAdded: false, // 포스트 업로드 성공
+};
+
+const dummyPost = {
+  User: {
+    id: 1,
+    nickname: "닉네임",
+  },
+  content: "더미 게시글",
 };
 
 export const LOAD_MAIN_POSTS_REQUEST = "LOAD_MAIN_POSTS_REQUEST";
@@ -65,6 +74,22 @@ const reducer = (state = initialState, action) => {
     case ADD_POST_REQUEST:
       return {
         ...state,
+        isAddingPost: true,
+        addPostErrorReason: "",
+        postAdded: false,
+      };
+    case ADD_POST_SUCCESS:
+      return {
+        ...state,
+        isAddingPost: false,
+        mainPosts: [dummyPost, ...state.mainPosts],
+        postAdded: true,
+      };
+    case ADD_POST_FAILURE:
+      return {
+        ...state,
+        isAddingPost: false,
+        addPostErrorReason: action.error,
       };
     default:
       return { ...state };
--- front/sagas/post.js
+++ front/sagas/post.js
@@ -1,5 +1,29 @@
-import { all } from "redux-saga/effects";
+import { all, delay, fork, put, takeLatest } from "redux-saga/effects";
+import {
+  ADD_POST_FAILURE,
+  ADD_POST_REQUEST,
+  ADD_POST_SUCCESS,
+} from "../reducers/post";
+
+function* addPost() {
+  try {
+    // yield axios.post();
+    yield delay(2000);
+    yield put({
+      type: ADD_POST_SUCCESS,
+    });
+  } catch (e) {
+    yield put({
+      type: ADD_POST_FAILURE,
+      error: e,
+    });
+  }
+}
+
+function* watchAddPost() {
+  yield takeLatest(ADD_POST_REQUEST, addPost);
+}
 
 export default function* postSage() {
-  yield all([]);
+  yield all([fork(watchAddPost)]);
 }
