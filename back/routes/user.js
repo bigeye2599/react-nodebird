983f69cd183a21add7001a848774b69c8be1c6ae
bigeye2599 <bigeye2599@gmail.com>
2021-05-31 22:15:33 +0900

    5-7. 회원가입 컨트롤러 만들기

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,9 +1,35 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const db = require("../models");
 
 const router = express.Router();
 
+// /api/user
+
 router.get("/", (req, res) => {});
-router.post("/", (req, res) => {});
+router.post("/", async (req, res, next) => {
+  try {
+    const exUser = await db.User.findOne({
+      where: {
+        userId: req.body.userId,
+      },
+    });
+    if (exUser) {
+      return res.status(403).send("이미 사용중인 아이디입니다.");
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+    const newUser = await db.User.create({
+      nickname: req.body.nickname,
+      userId: req.body.userId,
+      password: hashedPassword,
+    });
+    console.log(newUser);
+    return res.status(200).json(newUser);
+  } catch (e) {
+    console.error(e);
+    return next(e);
+  }
+});
 router.get(":id", (req, res) => {});
 router.post("/logout", (req, res) => {});
 router.post("/logout", (req, res) => {});
