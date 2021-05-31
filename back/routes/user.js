0f8552b4373cb0ac7ac7671d15026e7c95c427f6
bigeye2599 <bigeye2599@gmail.com>
2021-05-31 21:55:15 +0900

    5-6. 백엔드 서버 API 만들기

---
+++ back/routes/user.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {});
+router.post("/", (req, res) => {});
+router.get(":id", (req, res) => {});
+router.post("/logout", (req, res) => {});
+router.post("/logout", (req, res) => {});
+router.get("/:id/follow", (req, res) => {});
+router.post("/:id/follow", (req, res) => {});
+router.delete("/:id/follow", (req, res) => {});
+router.delete("/:id/follower", (req, res) => {});
+router.get("/:id/posts", (req, res) => {});
+
+module.exports = router;
